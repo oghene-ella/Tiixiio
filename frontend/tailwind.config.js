beb6eaa462d9d246ab887a4e506d8f8242c7a02e
oghene-ella <oghenekaro57@gmail.com>
2024-02-20 00:24:02 -0600

    custom styles, installed react-router-dom, and made some changes to the index.html file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/components/layout/*.{jsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        my_font: ["Oswald", "sans-serif"],
+        roboto_font: ["Roboto", "sans-serif"]
+      },
+      backgroundImage: {
+        'header': "url('./public/header.svg')",
+        "sideImg": "url('./public/contact_left.svg')",
+      },
+      colors: {
+        "bg_color": "#fdfdfd"
+      }
+    },
   },
   plugins: [],
 }
