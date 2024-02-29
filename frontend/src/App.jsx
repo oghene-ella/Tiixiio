58e92eeb2f17a28e95c6048202d21bbb76eb8ae1
oghene-ella <oghenekaro57@gmail.com>
2024-02-28 20:23:02 -0600

    added the footer to the app.jsx, modified the global css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Navbar from "./components/layout/Navbar";
 import Homepage from "../src/pages/Home"
+import Footer from './components/layout/Footer';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/about"  element={<Homepage />}/>
         </Routes>
       </div>
+      <Footer/>
     </>
   );
 };
