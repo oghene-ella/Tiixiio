a728b48f0cf0b54deff674619f49316a8a40d562
oghene-ella <oghenekaro57@gmail.com>
2024-03-13 14:58:38 -0500

    responsiveness

--- frontend/src/pages/Explore/Explore.jsx
+++ frontend/src/pages/Explore/Explore.jsx
@@ -5,9 +5,9 @@ const Explore = () => {
   return (
     <section className="bg-gray-50 flex flex-col gap-5 items-center justify-center py-10">
         <main className="w-10/12 flex flex-col gap-16 ">
-            <section className="flex justify-between items-center w-full">
+            <section className="flex flex-col md:flex-row justify-between items-center w-full gap-8">
                 <h1 className="text-3xl font-semibold underline decoration-wavy decoration-green-400">Explore States</h1>
-                <span className="w-3/4">
+                <span className="w-full md:w-2/3">
                     <SearchTag onSearch={(query) => console.log("Search query:", query)} />
                 </span>
             </section>
