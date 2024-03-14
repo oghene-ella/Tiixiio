cffb9ebe413db250213dd45a345ed06993451d82
oghene-ella <oghenekaro57@gmail.com>
2024-03-14 13:14:13 -0500

    worked on the states api and created some interface

---
+++ backend/src/interface/region.interface.ts
@@ -0,0 +1,11 @@
+export interface Region {
+  id?: string;
+  name: string;
+  states: string[];
+  population: number;
+  area: string;
+  description: string;
+  languagesSpoken: string[];
+  majorCities: string[];
+  ethnicGroups: string[];
+}
---
+++ backend/src/interface/states.interface.ts
@@ -0,0 +1,7 @@
+export interface Region {
+  id?: string;
+  name: string;
+  regionId: string;
+  regionName: string;
+  capital: string;
+}
