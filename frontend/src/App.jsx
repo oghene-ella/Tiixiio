8113b44d943a503a7f8d55f628b7850d30642f5f
oghene-ella <oghenekaro57@gmail.com>
2024-03-21 20:51:01 -0500

    added a protected route for the users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { baseUrl } from "./config"
 
 import Navbar from "./components/layout/Navbar";
 import Homepage from "./pages/Home";
@@ -10,8 +13,35 @@ import Explore from './pages/Explore/Explore';
 
 const App = () => {
   const location = useLocation();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const isAuthPage = location.pathname === '/login'  || location.pathname === '/sign_up' || location.pathname === '/dashboard';
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+  
+    if (token) {
+      axios.get(`${baseUrl}/auth/check-auth`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        console.log(response)
+        setIsAuthenticated(response.data.isAuthenticated);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false); 
+      });
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
+  
+
+  const isAuthPage = location.pathname === '/login'  || location.pathname === '/sign_up' || location.pathname === '/user/dashboard';
 
   return (
     <>
@@ -20,7 +50,7 @@ const App = () => {
         <Routes>        
           <Route path="/"  element={<Homepage/>} />
           <Route path="/explore"  element={<Explore/>} />
-          <Route path="/dashboard"  element={<Dashboard />}/>
+          <Route path="/user/dashboard"  element={isLoading ? <div>Loading...</div> : isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} />
           <Route path="/login"  element={<Login />}/>
           <Route path="/sign_up"  element={<SignUp />}/>
 
